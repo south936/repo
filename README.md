fe841d1e4704b1f909b23c09b67f7847fd5f4ef2
south936 <1624473537@qq.com>
2020-11-03 22:31:26 +0800

    vue

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 1. 输入框输入内容,点击新增
+### 2. 删除图标点击删除
+### 3. 多选框点击状态为打钩状态为 已完成
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,132 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <el-row>
+      <el-col :span="12" :offset="6">
+        <el-card>
+          <el-container>
+            <el-header>
+              <el-input v-model="toDoSth" size="small"></el-input>
+              <el-button
+                type="success"
+                @click="add()"
+                size="small"
+                icon="el-icon-edit"
+                >新增</el-button
+              >
+            </el-header>
+            <el-main>
+              <div class="left" v-if="list.toString() !== ''">
+                <el-row v-for="(item, index) in list" :key="index">
+                  <el-col :span="2" :class="item.state ? 'yes' : ''">
+                    <span @click="changeState(index, item.state)">
+                      <el-checkbox v-model="item.state"></el-checkbox
+                    ></span>
+                  </el-col>
+                  <el-col :span="12" :offset="1">{{ item.content }}</el-col>
+                  <el-col :span="4">{{ item.createTime }}</el-col>
+                  <el-col :span="5">
+                    <i
+                      class="el-icon-delete red"
+                      size="mini"
+                      @click="del(index)"
+                    ></i>
+                  </el-col>
+                </el-row>
+              </div>
+              <el-alert
+                width="85%"
+                class="infoText"
+                v-if="list.toString() === ''"
+                title="所有的事情都做完了"
+                :closable="false"
+              >
+              </el-alert>
+            </el-main>
+          </el-container>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      toDoSth: "",
+      list: [],
+      finishList: [],
+    };
+  },
+  methods: {
+    add() {
+      console.log(this.toDoSth);
+      if (this.toDoSth == "") {
+        return this.$message.error("请输入代办事件");
+      }
+      this.list.unshift({
+        state: false,
+        content: this.toDoSth,
+        createTime: new Date().toLocaleTimeString(),
+      });
+    },
+    del(index) {
+      this.list.splice(index, 1);
+      console.log(this.list);
+    },
+    changeState(index, state) {
+      this.list[index].state = !state;
+      console.log(this.list[index].state);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+<style lang="scss" scoped>
+.container {
+  // height: 100vh;
+  .el-row {
+    .el-header {
+      height: 10vh;
+      .el-input {
+        width: 50%;
+        margin-left: 10%;
+      }
+      .el-button {
+        // width: 20%;
+      }
+    }
+    .el-main {
+      height: 80vh;
+      place-content: center;
+      place-items: center;
+      .left {
+        // float: left;
+        width: 98%;
+        padding: 2%;
+        // background: #000;
+        .el-row > * {
+          height: 10%;
+          // line-height: 10%;
+          .yes {
+            color: #8c8c8c;
+          }
+          .red {
+            color: red;
+          }
+        }
+      }
+      .infoText {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+      .el-alert {
+        width: 40%;
+      }
+    }
+  }
 }
 </style>
